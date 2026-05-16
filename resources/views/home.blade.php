8e5dd5c64704967b3f7d47559b38fc85a560ad60
Muhammad Talha Saeed <italha.sbr@gmail.com>
2026-05-16 20:13:03 +0500

    refactor: remove call now button from home page hero section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -373,10 +373,6 @@
                 Get a Free Quote
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/></svg>
             </a>
-            <a href="tel:+923230625201" class="btn-outline-gold text-sm px-8 py-4">
-                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/></svg>
-                Call Now
-            </a>
         </div>
     </div>
 </section>
